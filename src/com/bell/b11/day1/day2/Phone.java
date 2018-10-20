4eab282b953a2a227f6cdba3532a8be328c07eb6
Jayasimha1 <jvootikuri001@my.wilmu.edu>
2018-10-19 23:30:57 -0400

    commit14

---
+++ src/com/bell/b11/day1/day2/Phone.java
@@ -0,0 +1,18 @@
+package com.bell.b11.day1.day2;
+
+public class Phone {
+    int imeiId;
+    double price;
+    String name;
+    String brand;
+    String simCardType;
+    int customerServiceNumber;
+    String problem;
+
+
+    void display(){
+
+        System.out.print(imeiId);
+    }
+
+}
