8777c59fa4f0453fef32083269ddef2d20764aed
Jayasimha1 <jvootikuri001@my.wilmu.edu>
2018-10-19 23:31:56 -0400

    commit15

---
+++ src/com/bell/b11/day1/day2/Student.java
@@ -0,0 +1,17 @@
+package com.bell.b11.day1.day2;
+
+public class Student {
+    int id;
+    double fee;
+    String name;
+    int roomNumber;
+
+
+    void display() {
+        System.out.println(id);
+
+    }
+
+}
+
+
