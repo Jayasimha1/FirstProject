f5474a244b36e977615e456434c1976971d33ef5
Jayasimha1 <jvootikuri001@my.wilmu.edu>
2018-10-19 23:37:10 -0400

    commit19

---
+++ src/com/bell/b11/day1/day3/PrimeNUmArrays.java
@@ -0,0 +1,31 @@
+package com.bell.b11.day1.day3;
+
+import java.util.Scanner;
+
+public class PrimeNUmArrays {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("please provide the count");
+        int count = scan.nextInt();
+
+        int[] userData = new int[count];
+
+        for (int k=0; k<userData.length;k++){
+
+
+        System.out.println("Give me" + k + "number");
+        userData[k] = scan.nextInt();
+    }
+        System.out.println("Thank you let me process");
+
+        for (int j=0; j<userData.length; j++){
+            boolean isPrime=true;
+
+        }
+
+
+    }
+}
+
+
