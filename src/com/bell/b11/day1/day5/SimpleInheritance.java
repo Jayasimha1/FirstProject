8a8de37cf14c93ed513264845527e9e37e69a984
Jayasimha1 <jvootikuri001@my.wilmu.edu>
2018-10-19 23:42:09 -0400

    commit23

---
+++ src/com/bell/b11/day1/day5/SimpleInheritance.java
@@ -0,0 +1,20 @@
+package com.bell.b11.day1.day5;
+
+public class SimpleInheritance {
+    public static void main(String[] args){
+
+    }
+}
+class A{
+    int a;
+    void bMtd(){
+
+    }
+}
+class B extends A{
+    int b;
+    void bMtd(){
+        System.out.println("inside B");
+    }
+
+}
