e774b8909e8bab03b0d2d88d5d9eeda813e4afa9
Jayasimha1 <jvootikuri001@my.wilmu.edu>
2018-10-19 23:32:47 -0400

    commit16

---
+++ src/com/bell/b11/day1/day2/Test.java
@@ -0,0 +1,13 @@
+package com.bell.b11.day1.day2;
+
+public class Test {
+    public static void main(String[] args){
+        Student s1= new Student();
+
+        s1.id=123;
+        s1.fee=1300.00;
+        s1.name="Jayasimha";
+        s1.roomNumber=215;
+               s1.display();
+}
+}
