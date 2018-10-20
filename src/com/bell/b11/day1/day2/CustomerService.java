a017d84748f1befa01eb52a3a0791f7ffb5589dd
Jayasimha1 <jvootikuri001@my.wilmu.edu>
2018-10-19 23:27:54 -0400

    commit12

---
+++ src/com/bell/b11/day1/day2/CustomerService.java
@@ -0,0 +1,10 @@
+package com.bell.b11.day1.day2;
+
+public class CustomerService {
+    public static void main (String[] args){
+        Phone s3=new Phone();
+        s3.customerServiceNumber=989898497;
+        s3.problem="simCArdError";
+        System.out.println(s3.problem);
+    }
+}
